5c3bc50ec9516286f164677095411541ed0fb017
Kevin van Zandvoort <Kevin.Van-Zandvoort@lshtm.ac.uk>
2021-01-20 18:32:10 +0000

    single run only for half run

--- single_run.R
+++ single_run.R
@@ -191,26 +191,34 @@ ibm_params_base <- list(
 model_seed <- r
 
 #scenario 0 - no lockdown or testing
-scen0_baseline <- as.data.table(covidIBM(getIVtype(ibm_params_base, test_coverage=c(), compliance_pos=1, compliance_hh=1), model_seed)$population)
+#scen0_baseline <- as.data.table(covidIBM(getIVtype(ibm_params_base, test_coverage=c(), compliance_pos=1, compliance_hh=1), model_seed)$population)
 
 #scenario 1 - no lockdown effect; 
 #scenario 1 - full compliance of household members
-scen1_test1_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=1, iv_stop=7*4), model_seed)
-scen1_test2_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=1, iv_stop=7*3), model_seed)
-scen1_test3_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=1, iv_stop=7*2), model_seed)
+#scen1_test1_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=1, iv_stop=7*4), model_seed)
+#scen1_test2_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=1, iv_stop=7*3), model_seed)
+#scen1_test3_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=1, iv_stop=7*2), model_seed)
 
 #scenario 1 - no compliance of household members
-scen1_test1_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0, iv_stop=7*4), model_seed)
-scen1_test2_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0, iv_stop=7*3), model_seed)
-scen1_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0, iv_stop=7*2), model_seed)
+#scen1_test1_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0, iv_stop=7*4), model_seed)
+#scen1_test2_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0, iv_stop=7*3), model_seed)
+#scen1_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0, iv_stop=7*2), model_seed)
+
+#scenario 1 - 50% compliance of household members
+scen1_test1_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*4), model_seed)
+scen1_test2_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*3), model_seed)
+scen1_test3_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*2), model_seed)
+
 
 #scenario 2 - lockdown brings Rt to 1
-time_test1 <- scen1_test1_compl_full$intervention[[1]]$start_t
+#time_test1 <- scen1_test1_compl_full$intervention[[1]]$start_t
+time_test1 <- scen1_test1_compl_half$intervention[[1]]$start_t
 target_Rt <- 1
 
 #calculate Rt in period before implementation of test
 rtHH = getRtHH(
-  as.data.table(scen1_test1_compl_full$population), 
+  #as.data.table(scen1_test1_compl_full$population), 
+  as.data.table(scen1_test1_compl_half$population), 
   #use longest period of possible infectious period to calculate Rt
   time_test1 + c(-ceiling(qgamma(
     0.999, shape=covid_parameters$dS$values$k,
@@ -223,48 +231,66 @@ if(rtHH["hh"] >= target_Rt){
 }
 
 #scenario 2 - lockdown without testing
-scen2_test0 <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(0), iv_stop=7*4, compliance_pos=0, compliance_hh=0, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test0 <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(0), iv_stop=7*4, compliance_pos=0, compliance_hh=0, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
 
 #scenario 2 - lockdown with tests and full compliance of household members
-scen2_test1_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=1, iv_stop=7*4, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
-scen2_test2_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=1, iv_stop=7*3, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
-scen2_test3_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=1, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test1_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=1, iv_stop=7*4, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test2_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=1, iv_stop=7*3, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test3_compl_full <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=1, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
 
 #scenario 2 - lockdown with tests and no compliance of household members
-scen2_test1_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0, iv_stop=7*4, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
-scen2_test2_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0, iv_stop=7*3, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
-scen2_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test1_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0, iv_stop=7*4, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test2_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0, iv_stop=7*3, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen2_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+
+#scenario 2 - lockdown with tests and no compliance of household members
+scen2_test1_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*4, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+scen2_test2_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*3, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+scen2_test3_compl_half <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=1, compliance_hh=0.5, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+
 
 #scenario 3 - lockdown brings Rt to 0.6, assume compliance with testing
 target_Rt <- 0.6
 if(rtHH["hh"] >= target_Rt){
   stop("HH transmission is too high")
 }
-scen3_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=0, compliance_hh=0, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
+#scen3_test3_compl_none <- covidIBM(getIVtype(ibm_params_base, test_coverage=c(prop_tested[c(1,2,3)]), compliance_pos=0, compliance_hh=0, iv_stop=7*2, lockdown_start = lockdown_start + time_test1, lockdown_effect=(target_Rt - rtHH["hh"])/rtHH["nohh"]), model_seed)
 
 #save results - infected individuals
-qs::qsave(scen0_baseline[part_id > 0], sprintf("%s/run_%s_scen0_baseline.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test0$population)[part_id > 0], sprintf("%s/run_%s_scen2_test0.qs", out_folder, r))
+#qs::qsave(scen0_baseline[part_id > 0], sprintf("%s/run_%s_scen0_baseline.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test0$population)[part_id > 0], sprintf("%s/run_%s_scen2_test0.qs", out_folder, r))
 
-qs::qsave(as.data.table(scen1_test1_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test1_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen1_test2_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test2_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen1_test3_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test3_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen1_test1_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test1_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen1_test2_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test2_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen1_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test3_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test1_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test1_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test2_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test2_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test3_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test3_compl_full.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test1_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test1_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test2_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test2_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen2_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test3_compl_none.qs", out_folder, r))
-qs::qsave(as.data.table(scen3_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen3_test3_compl_none.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test1_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test1_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test2_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test2_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test3_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen1_test3_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test1_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test1_compl_none.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test2_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test2_compl_none.qs", out_folder, r))
+#qs::qsave(as.data.table(scen1_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen1_test3_compl_none.qs", out_folder, r))
+
+qs::qsave(as.data.table(scen1_test1_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen1_test1_compl_half.qs", out_folder, r))
+qs::qsave(as.data.table(scen1_test2_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen1_test2_compl_half.qs", out_folder, r))
+qs::qsave(as.data.table(scen1_test3_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen1_test3_compl_half.qs", out_folder, r))
+
+#qs::qsave(as.data.table(scen2_test1_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test1_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test2_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test2_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test3_compl_full$population)[part_id > 0], sprintf("%s/run_%s_scen2_test3_compl_full.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test1_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test1_compl_none.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test2_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test2_compl_none.qs", out_folder, r))
+#qs::qsave(as.data.table(scen2_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen2_test3_compl_none.qs", out_folder, r))
+
+qs::qsave(as.data.table(scen2_test1_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen2_test1_compl_half.qs", out_folder, r))
+qs::qsave(as.data.table(scen2_test2_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen2_test2_compl_half.qs", out_folder, r))
+qs::qsave(as.data.table(scen2_test3_compl_half$population)[part_id > 0], sprintf("%s/run_%s_scen2_test3_compl_half.qs", out_folder, r))
+
+#qs::qsave(as.data.table(scen3_test3_compl_none$population)[part_id > 0], sprintf("%s/run_%s_scen3_test3_compl_none.qs", out_folder, r))
 
 #' Summarized results - test results in scenario
-tests <- sapply(scen3_test3_compl_none$intervention, "[[", "start_t")
+#tests <- sapply(scen3_test3_compl_none$intervention, "[[", "start_t")
+tests <- sapply(scen2_test3_compl_half$intervention, "[[", "start_t")
 out <- rbindlist(lapply(c(1:2), function(a){
   rbindlist(lapply(c(1:3), function(b, a){
-    rbindlist(lapply(c("full", "none"), function(d, b, a){
+    #rbindlist(lapply(c("full", "none"), function(d, b, a){
+    rbindlist(lapply(c("half"), function(d, b, a){
       iv = get(sprintf("scen%s_test%s_compl_%s", a, b, d))$intervention
       z = as.data.table(iv[b])[, test := b][, compliance := d]
       z[, variable := c("t_start", "test_attend", "test_positive", "in_quarantine", "susceptibles", "preinfectious", "infectious")];
@@ -275,43 +301,43 @@ out <- rbindlist(lapply(c(1:2), function(a){
   }, a))
 }))
 
-out_baseline <- rbindlist(lapply(1:3, function(x, tests){
-  rbindlist(lapply(1:2, function(d, dats, x, tests){
-    dat <- get(dats[d])
-    if(!is.data.table(dat)){
-      dat <- as.data.table(dat$population)
-    }
-    return(data.table(
-      "scenario" = ifelse(d==1, 0, d),
-      "compliance" = NA,
-      "test" = 0,
-      "in_quarantine" = NA,
-      "infectious" = dat[part_id > 0 & infectious_at <= tests[x] & recovered_at > tests[x], .N],
-      "preinfectious" = dat[part_id > 0 & infected_at <= tests[x] & infectious_at > tests[x], .N],
-      "susceptibles" = sum(bootstrap_population_popsize) - scen0_baseline[part_id > 0 & infected_at <= tests[x], .N],
-      "t_start" = tests[x],
-      "test_attend" = NA,
-      "test_positive" = NA
-    ))
-  }, c("scen0_baseline", "scen2_test0"), x, tests))
-}, tests))
+#out_baseline <- rbindlist(lapply(1:3, function(x, tests){
+#  rbindlist(lapply(1:2, function(d, dats, x, tests){
+#    dat <- get(dats[d])
+#    if(!is.data.table(dat)){
+#      dat <- as.data.table(dat$population)
+#    }
+#    return(data.table(
+#      "scenario" = ifelse(d==1, 0, d),
+#      "compliance" = NA,
+#      "test" = 0,
+#      "in_quarantine" = NA,
+#      "infectious" = dat[part_id > 0 & infectious_at <= tests[x] & recovered_at > tests[x], .N],
+#      "preinfectious" = dat[part_id > 0 & infected_at <= tests[x] & infectious_at > tests[x], .N],
+#      "susceptibles" = sum(bootstrap_population_popsize) - scen0_baseline[part_id > 0 & infected_at <= tests[x], .N],
+#      "t_start" = tests[x],
+#      "test_attend" = NA,
+#      "test_positive" = NA
+#    ))
+#  }, c("scen0_baseline", "scen2_test0"), x, tests))
+#}, tests))
 
-iv = scen3_test3_compl_none$intervention
-out_scen3 = rbindlist(lapply(1:3, function(x, iv){
-  z <- as.data.table(iv[[x]])
-  colnames(z) <- c("t_start", "test_attend", "test_positive", "in_quarantine", "susceptibles", "preinfectious", "infectious")
-  z[, test := x][, compliance := "none"][, scenario := 3]
-  return(z)
-}, iv))
+#iv = scen3_test3_compl_none$intervention
+#out_scen3 = rbindlist(lapply(1:3, function(x, iv){
+#  z <- as.data.table(iv[[x]])
+#  colnames(z) <- c("t_start", "test_attend", "test_positive", "in_quarantine", "susceptibles", "preinfectious", "infectious")
+#  z[, test := x][, compliance := "none"][, scenario := 3]
+#  return(z)
+#}, iv))
 
 #' combine summarized results
-out <- rbindlist(list(
-  out, out_baseline, out_scen3
-), use.names = TRUE)
+#out <- rbindlist(list(
+#  out, out_baseline, out_scen3
+#), use.names = TRUE)
 out[, popsize := sum(bootstrap_population_popsize)]
-out[, rt := rt]
+out[, rt := sum(rtHH)]
 setorder(out, scenario, compliance, test)
 
 #' save data
-fwrite(out, sprintf("%s/run_%s.csv", out_folder, r))
+fwrite(out, sprintf("%s/run_%s_half.csv", out_folder, r))
   
